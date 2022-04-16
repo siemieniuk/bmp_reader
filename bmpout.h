3a4c214565f3303615081f74da7cdcc9a73cd54d
Szymon Siemieniuk <szymonsiemieniuk01@gmail.com>
2022-04-17 01:18:49 +0200

    Documentation

--- bmpout.h
+++ bmpout.h
@@ -1,14 +1,25 @@
 #ifndef BMPOUT_H_INCLUDED
 #define BMPOUT_H_INCLUDED
 #include "bmp.h"
+
 /**
- * Prints specific file's header
+ * @brief Exports 
+ * 
+ * @param bmp BMPfile object to export
+ * @param path A path to the resulting BMP file
+ */
+void export_bitmap(const BMPfile* bmp, const char path[]);
+
+/**
+ * @brief Prints specific file's header
+ * 
  * @param f Object to print
  */
 void print_bitmap_file_header(struct tagBITMAPFILEHEADER* f);
 
 /**
- * Prints specific file's info
+ * @brief Prints specific file's info
+ * 
  * @param f Object to print
  */
 void print_bitmap_info_header(struct tagBITMAPINFOHEADER* f);
