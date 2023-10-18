3e54a38743c771dfa0c1e8b0fca06b2d9620e5a3
Szymon Siemieniuk <36564363+siemieniuk@users.noreply.github.com>
2023-10-18 23:40:53 +0200

    Comments formatting

--- bmpin.c
+++ bmpin.c
@@ -18,19 +18,23 @@ BMPfile* read_bmp_file(const char* const path) {
         fprintf(stderr, "File %s not found\n", path);
         exit(1);
     }
-    // creating IMG object
+
+    // create IMG object
     BMPfile* img = (BMPfile*)malloc(sizeof(BMPfile));
     img->file_header = read_bitmap_file_header(file);
     img->info_header = read_bitmap_info_header(file);
     img->row_length = compute_bmp_row_size(img);
-    // creating 2D array
+
+    // create 2D array
     img->pxarray = (unsigned char**)calloc(img->info_header->biHeight, sizeof(unsigned char*));
     for (unsigned i=0; i<img->info_header->biHeight; i++) {
         img->pxarray[i] = (unsigned char*)calloc(img->row_length, sizeof(unsigned char));
     }
+
     // Set the file position indicator in front of the pixel array
     fseek(file, img->file_header->bfOffBits, SEEK_SET);
-    // Reading each of a GRB pixel
+    
+    // Read each of a GRB pixel
     for (unsigned i=0; i<img->info_header->biHeight; i++) {
         fread(img->pxarray[i], img->row_length, 1, file);
     }
